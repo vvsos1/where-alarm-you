feee637e9f8a43621c1204766da2bad3442c909d
vvsos1 <vvsos10@naver.com>
2020-11-30 22:39:29 +0900

    LocationService에 현 위치 받아오는 기능 추가
    Google Play Service에서 제공하는 기능 이용

--- app/src/main/java/kr/ac/ssu/wherealarmyou/location/Address.java
+++ app/src/main/java/kr/ac/ssu/wherealarmyou/location/Address.java
@@ -5,25 +5,36 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import reactor.util.annotation.NonNull;
+import reactor.util.annotation.Nullable;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Address
-{
+public class Address {
+    @Nullable
     // 주소를 대표하는 이름; ex) 숭실대학교 정보과학관
-    String title;
-    
+            String title;
+
+    @Nullable
     // 도로명 주소
-    String roadAddress;
-    
+            String roadAddress;
+
+    @Nullable
     // 지번 주소
-    String jibunAddress;
-    
+            String jibunAddress;
+
+    @NonNull
     // 경도; X value
-    Double longitude;
-    
+            Double longitude;
+
+    @NonNull
     // 위도; Y value
-    Double latitude;
+            Double latitude;
+
+    public Address(@NonNull Double longitude, @NonNull Double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 }
